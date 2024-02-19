61d86a7afb70ebce6292152721c17b4b84a979ab
Murilo Barreto <barreto_11almeida@hotmail.com>
2024-02-18 21:38:30 -0300

    Aplicando sistema de colisao nos blocos

--- game01/src/com/mbstudios/main/Game.java
+++ game01/src/com/mbstudios/main/Game.java
@@ -27,8 +27,8 @@ public class Game extends Canvas implements Runnable, KeyListener{
 	public static JFrame frame;
 	private boolean isRunning;
 	private Thread thread;
-	private final int WIDTH = 240;
-	private final int HEIGHT = 160;
+	public static final int WIDTH = 240;
+	public static final int HEIGHT = 160;
 	private final int SCALE = 3;
 	
 	private BufferedImage image;
